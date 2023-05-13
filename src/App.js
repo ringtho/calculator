2d8baca4a63712801a13c7035f5e67ec7a8182ab
Smith Ringtho <sringtho@gmail.com>
2023-05-13 10:39:07 +0300

    Fixed the UI of the calculator

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function App () {
+  const [value, setValue] = useState(0)
   return (
-    <div className="App">
-      <h1>Smith Ringtho</h1>
+    <div className="app">
+      <div className='calculator-container'>
+        <div className='display-section'>
+          <div className='formular-screen'></div>
+          <div id="display" className='working-screen'>{value}</div>
+        </div>
+        <div className='buttons-container'>
+          <div id="clear">AC</div>
+          <div id="divide">/</div>
+          <div id="multiply">x</div>
+          <div id="seven">7</div>
+          <div id="eight">8</div>
+          <div id="nine">9</div>
+          <div id="subtract">-</div>
+          <div id="four">4</div>
+          <div id="five">5</div>
+          <div id="six">6</div>
+          <div id="add">+</div>
+          <div id="one">1</div>
+          <div id="two">2</div>
+          <div id="three">3</div>
+          <div id="equals">=</div>
+          <div id="zero">0</div>
+          <div id="decimal">.</div>
+          {/* <div id="equals">=</div> */}
+        </div>
+      </div>
+      <div className='footer'>
+        <p>Designed an Coded By</p>
+        <p className='name'>Smith Ringtho</p>
+      </div>
     </div>
   )
 }
